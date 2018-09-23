bc4d7eac71b5c5206a2b946bbe1799e92ea81c83
baiye <baiye.csh@dtyunxi.com>
2018-09-23 13:28:48 +0800

    feat(引入右侧属性框):

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import BpmnModeler from 'bpmn-js/lib/Modeler';
+import propertiesPanelModule from 'bpmn-js-properties-panel';
+import propertiesProviderModule from 'bpmn-js-properties-panel/lib/provider/camunda';
+import camundaModdleDescriptor from 'camunda-bpmn-moddle/resources/camunda';
 import 'normalize.css'
 import './index.less'
 import 'bpmn-js/dist/assets/diagram-js.css';
 import 'bpmn-font/dist/css/bpmn-embedded.css';
+import "bpmn-js-properties-panel/styles/properties.less";
 
 let bpmnModeler = new BpmnModeler({
-  container: '#bpmn-canvas'
+  container: '#bpmn-canvas',
+  propertiesPanel: {
+    parent: '#properties-panel-parent',
+  },
+  additionalModules: [
+    propertiesPanelModule,
+    propertiesProviderModule
+  ],
+  moddleExtensions: {
+    camunda: camundaModdleDescriptor
+  }
 });
 bpmnModeler.createDiagram()
